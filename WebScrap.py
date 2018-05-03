d5941b6720c46d673bffe08ca7b87a53dd4321d0
Amzar <am.indie96@gmail.com>
2018-05-03 13:29:44 +0800

    py file
    reupdate request link and code

--- WebScrap.py
+++ WebScrap.py
@@ -1,18 +1,18 @@
 
 # coding: utf-8
 
-# In[172]:
+# In[15]:
 
 
 import requests
 import bs4
 from bs4 import BeautifulSoup as soup
 
-url = requests.get('http://shop.abstraxjingga.com/webshaper/store/newProducts.asp')
+url = requests.get('http://shop.abstraxjingga.com/webshaper/store/bestSellers.asp')
 soup = soup(url.content, 'html.parser')
 
 
-# In[173]:
+# In[2]:
 
 
 new_arrival = soup.find(id="col1")
@@ -21,50 +21,46 @@ item_name = new_arrival.find_all(class_="prodItemName")
 best = item_name[0:3]
 
 
-# In[174]:
+# In[3]:
 
 
 print(best)
 
 
-# In[175]:
+# In[4]:
 
 
 findcat = soup.find(id="col2Left").get_text()
-#ttesting commit
 
-# In[176]:
+
+# In[5]:
 
 
 print(findcat)
 
 
-# In[177]:
+# In[6]:
 
 
 findcat = soup.find(id="col2Left")
 find_second_cat = findcat.find(class_="categoryList1 cat-id-82").get_text()
 
 
-# In[178]:
+# In[7]:
 
 
 print(find_second_cat)
 
 
-# In[218]:
+# In[16]:
 
+#UPDATE 3 may
 
 new_arrival = soup.findAll("div", {"class": "prodItemName"})
 new_arrival2 = soup.findAll("div", {"class": "prodItemPrice"})
-a = new_arrival+new_arrival2
-
-
-for i in new_arrival:
-    itext = i.get_text(" ")
-    #print(itext)
-    for j in new_arrival2:
-        jtext = j.get_text(" ")
-        #print(jtext)
-    print("Product name : {} and price : {}".format(itext, jtext))
+    
+for x in range (len(new_arrival)):
+    name=new_arrival[x].get_text()
+    price=new_arrival2[x].get_text()
+    print(name+" and price "+price)
 
